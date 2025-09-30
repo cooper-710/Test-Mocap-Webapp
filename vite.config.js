fe624b67b4d6dfb5b0d00edbb4151323aa847feb
cooper-710 <cooprobinson7@gmail.com>
2025-09-30 13:18:02 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-// vite.config.js
+// vite.config.ts (root)
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// Your repo slug:
 export default defineConfig({
   plugins: [react()],
-  base: "/Test-Mocap-Webapp/",   // <-- GH Pages base path
+  base: "/Test-Mocap-Webapp/",
   build: { outDir: "dist" }
 });
